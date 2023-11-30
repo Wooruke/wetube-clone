a7501f10e040d62c797931dcc3b70c7b5cd9615b
wooruke <jws4799@naver.com>
2023-11-30 20:04:11 +0900

    init

---
+++ src/routers/videoRouter.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  watch,
+  getEdit,
+  postEdit,
+  deleteVideo,
+  upload,
+} from "../controllers/videoController";
+
+const videoRouter = express.Router();
+
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+
+export default videoRouter;
